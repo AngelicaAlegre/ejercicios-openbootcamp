5cbe2094c1626735b9ea9d859417a216d1d3c77b
AngelicaAlegre <angelicaalegre802@gmail.com>
2022-09-01 16:22:10 -0300

    resolucion ejercicio3
    se pidio crear variables y un diccionario e imprinirlos

---
+++ ejercicio3-openbootcamp.py
@@ -0,0 +1,11 @@
+varnombre = 'Elias' 
+varapellido = 'Delgado'
+varedad = '21'
+varemail = 'delgadoelias@gmail.com' 
+vartelefono = '3764998741'
+varamigos = 'Marcos'
+varcasado = False
+varhijos = False
+mi_diccionario = {'Peliculas Vistas': 'No Respires'}
+print(varnombre, varapellido, varedad, varemail, vartelefono, varamigos, varcasado, varhijos)
+print(mi_diccionario)
